f987fbefdcf47686874e12497a73f269b4535f94
Rajpateriya <pateriyaraj52@gmail.com>
2024-07-16 02:05:22 +0530

    BLogs and footer created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // src/App.js
 import React from 'react';
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 import './App.css';
-import Partners from './components/Partners';
-import Header from './components/Header';
+import Partners from './components/Partners/Partners';
+import Header from './components/Header/Header';
+import Blogs from './components/Blogs/Blog';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main className="p-4">
         <Partners/>
         <p>Here goes the main content of your application.</p>
+        <Blogs/>
       </main>
       <Footer />
     </div>
